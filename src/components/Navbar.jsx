654287e2a33b3d2273dfdbcab2af8ea0aba2be53
Alek30k <alemank23k@gmail.com>
2024-12-27 15:51:15 -0300

    last commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,19 @@
 const Navbar = () => {
-  return <div className="">Navbar</div>;
+  return (
+    <div className="">
+      {/* LOGO */}
+      <div className="">
+        <img src="/logo.png" alt="logo" className="w-8 h-8" />
+        <span>Alelog</span>
+      </div>
+      {/* MOBILE MENU */}
+      <div className=""></div>
+      {/* MOBILE BUTTON */}
+      {/* MOBILE LINK LIST */}
+      {/* DESKTOP MENU */}
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Navbar;
