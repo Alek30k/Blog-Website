78c2e26edabd17feca48cc46c8e121ee2603f086
Alek30k <alemank23k@gmail.com>
2024-12-27 16:18:50 -0300

    last commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="">
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* LOGO */}
-      <div className="">
+      <div className="flex items-center gap-4 text-2xl font-bold">
         <img src="/logo.png" alt="logo" className="w-8 h-8" />
         <span>Alelog</span>
       </div>
       {/* MOBILE MENU */}
-      <div className=""></div>
+      <div className="md:hidden">
+        <div className="cursor-pointer text-4xl">{open ? "X" : "☰"}</div>
+      </div>
       {/* MOBILE BUTTON */}
       {/* MOBILE LINK LIST */}
       {/* DESKTOP MENU */}
-      <div className=""></div>
+      <div className="hidden md:flex">D</div>
     </div>
   );
 };
