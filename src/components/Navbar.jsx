ebb8af9c1ecc91e9f2275e665c970d91b070bc07
Alek30k <alemank23k@gmail.com>
2025-03-15 22:05:59 -0300

    last commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -14,7 +15,7 @@ const Navbar = () => {
       <div className="md:hidden">
         {/* MOBILE BUTTON */}
         <div
-          className="cursor-pointer text-4xl"
+          className="cursor-pointer text-4xl "
           onClick={() => setOpen((prev) => !prev)}
         >
           {open ? "X" : "☰"}
@@ -22,14 +23,32 @@ const Navbar = () => {
       </div>
       {/* MOBILE LINK LIST */}
       <div
-        className={`w-full h-screen flex flex-col items-center justify-center absolute top-16  transition-all ease-in-out bg-red-500 ${
+        className={`w-full h-screen bg-[#e6e6ff] flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
           open ? "-right-0" : "-right-[100%]"
         }`}
       >
         Menu
       </div>
       {/* DESKTOP MENU */}
-      <div className="hidden md:flex">D</div>
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <Link to="/">Home</Link>
+        <Link to="/posts?sort=trending">Trending</Link>
+        <Link to="/posts?sort=popular">Most Popular</Link>
+        <Link to="/">About</Link>
+        <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+          Login 👋
+        </button>
+        {/* <SignedOut>
+          <Link to="/login">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn> */}
+      </div>
     </div>
   );
 };
