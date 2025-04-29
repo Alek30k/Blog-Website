79c7b2ddc9f2ee18f62584afa69d27d2c8e05337
Alek30k <alemank23k@gmail.com>
2025-04-29 17:00:52 -0300

    last commit

--- src/components/FeacturedPost.jsx
+++ src/components/FeacturedPost.jsx
@@ -41,8 +41,14 @@ const FeacturedPost = () => {
             <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
               <h1 className="font-semibold">02.</h1>
               <Link className="text-blue-800">postscategory</Link>
-              <span className="text-gray-500 text-sm">posts</span>
+              <span className="text-gray-500 text-sm">2 days ago</span>
             </div>
+            <Link
+              to="/test"
+              className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </Link>
           </div>
         </div>
         {/* third */}
